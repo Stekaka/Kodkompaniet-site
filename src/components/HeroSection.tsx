0e03df5807e03be0fba11ceb2888630302526e47
Oliver Carlsson <oliverc@hotmail.se>
2025-06-19 23:25:49 +0200

    Din commit-meddelande

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion'
 import { Typewriter } from 'react-simple-typewriter'
 import { useState } from 'react'
-import ContactForm from './ContactForm.tsx'
+import ContactForm from './ContactForm'
 
 export default function HeroSection() {
   const [showForm, setShowForm] = useState(false)

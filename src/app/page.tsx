83890f0a5d58501909eb7cfd12eb3b79be0ad692
Oliver Carlsson <oliverc@hotmail.se>
2025-06-19 23:23:12 +0200

    Din commit-meddelande

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Canvas } from '@react-three/fiber'
 import { useEffect } from 'react'
 import PricingSection from '@/components/PricingSection'
 import HeroSection from '@/components/HeroSection'
@@ -8,8 +7,6 @@ import ProcessSection from '@/components/ProcessSection'
 import ContactSection from '@/components/ContactSection'
 import PortfolioSection from '@/components/PortfolioSection'
 import SpacePortalSectionV2 from '@/components/SpacePortalSectionV2'
-import SpaceBackgroundCanvas from '@/components/SpaceBackgroundCanvas'
-import SpacePortalStars from '@/components/SpacePortalStars'
 
 export default function Home() {
   useEffect(() => {
@@ -18,24 +15,9 @@ export default function Home() {
 
   return (
     <div className="relative">
-      <Canvas
-        className="fixed inset-0 w-full h-[200vh] z-0"
-        style={{
-          position: 'fixed',
-          inset: 0,
-          width: '100vw',
-          height: '200vh',
-          zIndex: 0,
-          pointerEvents: 'none',
-        }}
-        camera={{ position: [0, 0, 1] }}
-      >
-        <color attach="background" args={['black']} />
-        <SpacePortalStars progress={0} />
-      </Canvas>
       <div className="relative z-10">
         <HeroSection />
-        <SpacePortalSectionV2 hideStars />
+        <SpacePortalSectionV2 />
         <ProcessSection />
         <PricingSection />
         <ContactSection />
